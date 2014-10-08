cdf5018fa04d0e1c4d3f9f9e03a3fb1f0997a6dc
inablack11 <inablack11@gmail.com>
2014-10-08 17:00:07 +0300

    r

--- HibernatePlay/src/main/java/com/cmu/hibernate/App.java
+++ HibernatePlay/src/main/java/com/cmu/hibernate/App.java
@@ -1,13 +1,30 @@
 package com.cmu.hibernate;
 
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.cmu.hibernate.model.TestT1;
+import com.cmu.hibernate.util.HibernateUtil;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Maven + Hibernate + Oracle");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+ 
+		session.beginTransaction();
+		TestT1 user = new TestT1();
+ 
+		user.setUserId(100);
+		user.setUsername("u1");
+		user.setCreatedBy("s1");
+		user.setCreatedDate(new Date());
+ 
+		session.save(user);
+		session.getTransaction().commit();
+	}
 }
--- HibernatePlay/src/main/java/com/cmu/hibernate/util/HibernateUtil.java
+++ HibernatePlay/src/main/java/com/cmu/hibernate/util/HibernateUtil.java
@@ -10,7 +10,8 @@ public class HibernateUtil {
 	private static SessionFactory buildSessionFactory() {
 		try {
 			// Create the SessionFactory from hibernate.cfg.xml
-			return new Configuration().configure().buildSessionFactory();
+			Configuration config = new Configuration().configure();
+			return config.buildSessionFactory();
 		} catch (Throwable ex) {
 			// Make sure you log the exception, as it might be swallowed
 			System.err.println("Initial SessionFactory creation failed." + ex);
